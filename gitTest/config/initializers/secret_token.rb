eccc0c4d10cff0ab799475bb4e3ab2ac1034c235
Ben Taylor <ben@ben-HP-Compaq-nc6400-RY485EP-ABF.(none)>
2013-03-20 20:36:04 +0100

    first Git

---
+++ gitTest/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTest::Application.config.secret_token = 'd735ac536f981e68865dc7633c73bc5bb420aa06ce04659785eab394f6d9bda5c38902fd9aed7c6ae77f89024bba4915f30e12f0bb6af42b4a19fdf0fed59c4e'
